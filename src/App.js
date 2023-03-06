6341fdeee53a0bd068355230edfd48597e0fa27d
SY97P <onetuks@naver.com>
2023-03-06 14:20:39 +0900

    react class and extends React.Component basic lec done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div></div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  /*
+    componentDidMount()
+
+    - life cycle : Mounting < Component
+    - operation : fetching data
+  */
+  componentDidMount() {
+    setTimeout(() => {
+      // state를 추가하는 것도 자유임.
+      this.setState({ isLoading: false, book: true });
+    }, 6000);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>{isLoading ? "Loading" : "We are ready"}</div>
+    );
+  }
 }
 
 export default App;
